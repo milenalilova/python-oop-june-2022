fd86717ca9161024adac59df7e3475c11c635630
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-04-11 17:57:26 -0300

    Add files via upload

---
+++ classes_an_objects_exercise/spoopify_revision/band.py
@@ -0,0 +1,28 @@
+from classes_and_objects_exercise_revision.project.album import Album
+
+
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album):
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name: str):
+        for album in self.albums:
+            if album.name == album_name:
+                if album.published:
+                    return "Album has been published. It cannot be removed."
+                self.albums.remove(album)
+                return f"Album {album_name} has been removed."
+        return f"Album {album_name} is not found."
+
+    def details(self):
+        output = f"Band {self.name}\n"
+        for album in self.albums:
+            output += f"{album.details()}\n"
+        return output.strip()
