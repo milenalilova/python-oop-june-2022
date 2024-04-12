4c2dac8ac5034c0129188efc9751eaec25501f1c
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-04-12 18:35:53 -0300

    Add files via upload

---
+++ inheritance_lab/multilevel_inheritance_revision/project/car.py
@@ -0,0 +1,7 @@
+from multilevel_inheritance_revision.project.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    def drive(self):
+        return 'driving...'
+
---
+++ inheritance_lab/multilevel_inheritance_revision/project/sports_car.py
@@ -0,0 +1,6 @@
+from multilevel_inheritance_revision.project.car import Car
+
+
+class SportsCar(Car):
+    def race(self):
+        return 'racing...'
