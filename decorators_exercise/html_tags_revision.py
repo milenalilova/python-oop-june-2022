ccb1d0de7ac8eb4dcd162ea208c613b8edd7d3ca
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-04-23 09:21:48 -0300

    Add files via upload

--- decorators_exercise/html_tags_revision.py
+++ decorators_exercise/html_tags_revision.py
@@ -2,7 +2,7 @@ def tags(tag):
     def decorator(func):
         def wrapper(*args):
             result = func(*args)
-            return f"{tag}{result}{tag}"
+            return f"<{tag}>{result}</{tag}>"
 
         return wrapper
 
